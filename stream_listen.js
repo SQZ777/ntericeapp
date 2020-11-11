15685c47128cf107e078038e42a9f6d1030bc022
darren <darren_zhang@trendmicro.com>
2020-11-11 14:09:41 +0800

    update

--- stream_listen.js
+++ stream_listen.js
@@ -4,13 +4,11 @@ const http = require('http');
 //設定server網址，因為在本機端測試，所以輸入127.0.0.1
 const hostname = 'https://api.twitch.tv/helix/'  //上傳至伺服器需拿掉
 
-//port 號會由 Heroku 給予，因此不再自行指定
-const port = process.env.PORT || 3000;
-
 //新增一個server並指定他的頁面資訊，內容為'Hello World!'
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Client-ID', 'leelbgmv51u0sq5xoo9uutmf0pgfhq');
+    res.setHeader('Authorization', 'Bearer 0pa4ee6t2c4nvrrctjyt5noopvfhk1');
     res.end('Hello World!\n');
 });
 
