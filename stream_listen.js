e899d70666e5cf59addfd7a0046f5241b60c7e42
darren <darren_zhang@trendmicro.com>
2020-11-12 14:00:39 +0800

    ping pong login

--- stream_listen.js
+++ stream_listen.js
@@ -1,16 +1,28 @@
-//引用'http'模組
-const http = require('http');
+require('dotenv').config();
 
-//設定server網址，因為在本機端測試，所以輸入127.0.0.1
-const hostname = 'https://api.twitch.tv/helix/'  //上傳至伺服器需拿掉
+const {
+    getRequest
+} = require('./lib/request');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-//新增一個server並指定他的頁面資訊，內容為'Hello World!'
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Client-ID', 'leelbgmv51u0sq5xoo9uutmf0pgfhq');
-    res.setHeader('Authorization', 'Bearer 0pa4ee6t2c4nvrrctjyt5noopvfhk1');
-    res.end('Hello World!\n');
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.channels.cache.get("776035789108543528").send(`${client.user.tag} 前來報到`);
+    count = 0
+    setInterval(() => {
+        if (count < 3) {
+            client.channels.cache.get("776035789108543528").send("測試一下每三秒傳一次訊息傳三次");
+            count++;
+        }
+    }, 3000);
 });
 
-//監聽得到的 port 號開啟
-server.listen(port, () => console.log(`Listening on ${port}`));
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+
+client.login(process.env.discordToken);
