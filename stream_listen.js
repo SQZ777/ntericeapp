84aba3bccd9c246265980e524da53d23e71fd1d4
darren <darren_zhang@trendmicro.com>
2020-11-12 17:15:11 +0800

    stream notify complete

--- stream_listen.js
+++ stream_listen.js
@@ -5,12 +5,52 @@ const {
 } = require('./lib/request');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var channel_notify_dict = {
+    'morgantang': {
+        status: 'close',
+        notify_time: null,
+        close_time: null
+    },
+    'attackfromtaiwan': {
+        status: 'close',
+        notify_time: null,
+        close_time: null
+    },
+    'hsiny0903': {
+        status: 'close',
+        notify_time: null,
+        close_time: null
+    }
+}
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
     // client.channels.cache.get("776035789108543528").send(`${client.user.tag} 前來報到`);
-    
-    setInterval(() => {
+    setInterval(async () => {
+        for (const channel_name in channel_notify_dict) {
+            var channel_status_resp = await get_channel_status(channel_name)
+            console.log(channel_status_resp)
+            if (channel_status_resp === undefined) {
+                if (channel_notify_dict[channel_name].status === 'open') {
+                    channel_notify_dict[channel_name].close_time = new Date()
+                }
+                channel_notify_dict[channel_name].status = 'close'
+            } else {
+                now_time = new Date()
+
+                if (channel_notify_dict[channel_name].notify_time === null) {
+                    client.channels.cache.get("776035789108543528").send(`${channel_name} 開台啦!`);
+                } else {
+                    // image 320x180
+                    clostime_diff_with_notifytime = Math.abs(channel_notify_dict[channel_name].close_time - channel_notify_dict[channel_name].notify_time) / 1000 / 60
+                    if (clostime_diff_with_notifytime >= 120 && channel_notify_dict[channel_name].close_time !== null) {
+                        client.channels.cache.get("776035789108543528").send(`${channel_name} 開台啦!RRRRRRRRRRRR`);
+                    }
+                }
+                channel_notify_dict[channel_name].status = 'open';
+                channel_notify_dict[channel_name].notify_time = new Date(channel_status_resp.started_at);
+            }
+        }
     }, 3000);
 });
 
@@ -20,5 +60,16 @@ client.on('message', msg => {
     }
 });
 
+async function get_channel_status(channel_name) {
+    const resp = await getRequest({
+        url: `https://api.twitch.tv/helix/streams?user_login=${channel_name}`,
+        headers: {
+            'Client-ID': process.env.twitchClientId,
+            'Authorization': process.env.twitchAuthorization,
+        },
+        json: true,
+    });
+    return resp.body.data[0];
+}
 
 client.login(process.env.discordToken);
