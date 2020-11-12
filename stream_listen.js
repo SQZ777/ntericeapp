c68c06fe792e42457f7d97b290f1b70781d67953
darren <darren_zhang@trendmicro.com>
2020-11-12 14:28:25 +0800

    update stream listen

--- stream_listen.js
+++ stream_listen.js
@@ -8,13 +8,9 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.channels.cache.get("776035789108543528").send(`${client.user.tag} 前來報到`);
-    count = 0
+    // client.channels.cache.get("776035789108543528").send(`${client.user.tag} 前來報到`);
+    
     setInterval(() => {
-        if (count < 3) {
-            client.channels.cache.get("776035789108543528").send("測試一下每三秒傳一次訊息傳三次");
-            count++;
-        }
     }, 3000);
 });
 
