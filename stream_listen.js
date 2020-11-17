1de5851d6e57e9e44c154cb93aebbeb768b2e6bc
darren <darren_zhang@trendmicro.com>
2020-11-17 11:12:37 +0800

    update web

--- stream_listen.js
+++ stream_listen.js
@@ -9,9 +9,10 @@ var server = http.createServer(function (req, res) {   // 2 - 建立server
     }
 });
  
-server.listen(8877); //3 - 進入此網站的監聽 port, 就是 localhost:xxxx 的 xxxx
+server.listen(8877, '127.0.0.1', ()=>{
+    console.log(`Server running at http://127.0.0.1:8877/`);
+}); //3 - 進入此網站的監聽 port, 就是 localhost:xxxx 的 xxxx
  
-console.log('Node.js web server at port 5000 is running..')
 
 const {
     getRequest
