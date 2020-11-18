e2e128c0ae53f745d0cdeea069e4d23bb2ee2c6d
darren <darren_zhang@trendmicro.com>
2020-11-18 14:07:12 +0800

    move mongodb

--- mongodb.js
+++
@@ -1,9 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config()
-const uri = process.env.mongoHost;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(async err => {
-  const collection = await client.db("discord").collection("streamers");
-  console.log(await collection.findOne({name: "attackfromtaiwan"}))
-  client.close();
-});
