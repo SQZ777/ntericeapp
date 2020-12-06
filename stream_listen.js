a955133d904ca9d91548a0efde5209503be0bfd1
sqz777-at-342277909689 <icelffs@gmail.com>
2020-12-06 23:01:25 +0800

    remove server listen in strem_listen

--- stream_listen.js
+++ stream_listen.js
@@ -10,14 +10,6 @@ var channel_names = ['MorganTang', 'thisiceisfromtaiwan', 'hsiny0903', 'defponyt
 const http = require('http'); // 1 - 載入 Node.js 原生模組 http
 const { request_to_myself } = require('./lib/request_myself')
 
-const PORT = process.env.PORT || 3000
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World!');
-}).listen(PORT);
-
 setInterval(()=>{
     request_to_myself()
 },600000)
