b53823a64418d60d9aaa5f9d89394ab578ad33a1
sqz777-at-342277909689 <icelffs@gmail.com>
2020-12-07 09:24:36 +0800

    add server

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const http = require('http'); // 1 - 載入 Node.js 原生模組 http
 const PORT = process.env.PORT || 3000
 http.createServer(function (req, res) {
     if (req.url == '/streamer_notify') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end('Streamer Open!!!');
+        var body = ''
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
     }else{
         res.writeHead(200, {
             'Content-Type': 'text/plain'
