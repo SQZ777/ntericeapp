2d9a5f4a977681b43789ad03d631939596718a80
sqz777-at-342277909689 <icelffs@gmail.com>
2020-12-07 09:21:54 +0800

    push streamer open

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
 const http = require('http'); // 1 - 載入 Node.js 原生模組 http
 
 const PORT = process.env.PORT || 3000
 http.createServer(function (req, res) {
     if (req.url == '/streamer_notify') {
         res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end();
+        res.end('Streamer Open!!!');
     }else{
         res.writeHead(200, {
             'Content-Type': 'text/plain'
