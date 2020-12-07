b768655fd75804519e4e1b2821fcbd0460359016
sqz777-at-342277909689 <icelffs@gmail.com>
2020-12-07 09:28:31 +0800

    add post

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const http = require('http'); // 1 - 載入 Node.js 原生模組 http
 
 const PORT = process.env.PORT || 3000
 http.createServer(function (req, res) {
-    if (req.url == '/streamer_notify') {
+    if (req.method == 'POST' && req.url === '/streamer_notify') {
+        console.log('POST')
         var body = ''
-        request.on('data', chunk => {
-            body += chunk.toString();
-        });
-        request.on('end', () => {
-            callback(parse(body));
-        });
-    }else{
+        req.on('data', function(data) {
+          body += data
+          console.log('Partial body: ' + body)
+        })
+        req.on('end', function() {
+          console.log('Body: ' + body)
+          response.writeHead(200, {'Content-Type': 'text/html'})
+          response.end('post received' + body)
+        })
+      }else{
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
