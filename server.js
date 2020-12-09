6466c65b39dcffc0aaf5ec2e6c6a8dd1be6ca070
sqz777 <sqz777isme@gmail.com>
2020-12-10 01:10:10 +0800

    change to express

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-const http = require('http'); // 1 - 載入 Node.js 原生模組 http
+const express = require('express');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
-http
-  .createServer((req, res) => {
-    if (req.method === 'POST' && req.url === '/streamer_notify') {
-      console.log('POST');
-      let body = '';
-      req.on('data', (data) => {
-        body += data;
-        console.log(`Partial body: ${body}`);
-      });
-      req.on('end', () => {
-        console.log(`Body: ${body}`);
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`post received${body}`);
-      });
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/plain',
-      });
-      res.end('Hello World!');
-    }
-  })
-  .listen(PORT);
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  console.log(req.body); // your JSON
+  res.send(req.body); // echo the result back
+});
+
+app.post('/streamer_notify', (req, res) => {
+  res.send(req.body.user_name);
+});
+
+app.listen(PORT, () => {
+  console.log('Example app listening on port 3000!');
+});
