27d4e43a8d3d41246418b8e224f677d3fc028171
sqz777 <sqz777isme@gmail.com>
2020-12-10 00:06:20 +0800

    fix server.js format

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const http = require('http'); // 1 - 載入 Node.js 原生模組 http
 
-const PORT = process.env.PORT || 3000
-http.createServer(function (req, res) {
-    if (req.method == 'POST' && req.url === '/streamer_notify') {
-        console.log('POST')
-        var body = ''
-        req.on('data', function (data) {
-            body += data
-            console.log('Partial body: ' + body)
-        })
-        req.on('end', function () {
-            console.log('Body: ' + body)
-            res.writeHead(200, { 'Content-Type': 'text/plain' })
-            res.end('post received' + body)
-        })
+const PORT = process.env.PORT || 3000;
+http
+  .createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/streamer_notify') {
+      console.log('POST');
+      let body = '';
+      req.on('data', (data) => {
+        body += data;
+        console.log(`Partial body: ${body}`);
+      });
+      req.on('end', () => {
+        console.log(`Body: ${body}`);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`post received${body}`);
+      });
     } else {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('Hello World!');
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('Hello World!');
     }
-}).listen(PORT);
-
+  })
+  .listen(PORT);
