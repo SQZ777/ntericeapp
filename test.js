73d1f0f7d155e77a7f3fa1a90c3b3e30e8b4f617
darren <darren_zhang@trendmicro.com>
2020-12-09 14:56:47 +0800

    remove test

--- test.js
+++
@@ -1,9 +0,0 @@
-function go(client) {
-    client.channels.cache.get("776035789108543528").send('in test!');
-    client.channels.cache.get("776035789108543528").send('in test haha');
-    console.log('in go')
-}
-
-module.exports = {
-    go
-}
