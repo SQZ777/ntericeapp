b4847c0397482e11543195442e2e794ae0f2fdd0
sqz777 <sqz777isme@gmail.com>
2020-12-10 23:14:44 +0800

    add back express in app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 const Discord = require('discord.js');
+const { MongoDbBase } = require('./lib/mongodbBase');
 
 const Client = new Discord.Client();
 const igotalldayService = require('./lib/igotalldayYoutubeService');
@@ -78,3 +83,24 @@ Client.on('message', async (msg) => {
 });
 
 Client.login(process.env.discordToken);
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  console.log(req.body); // your JSON
+  res.send(req.body); // echo the result back
+});
+
+app.post('/streamer_notify', async (req, res) => {
+  const testCollection = new MongoDbBase('test');
+  await testCollection.connectMongo();
+  await testCollection.updateData(
+    { id: 1 },
+    { $set: { context: req.body.data[0].user_name } },
+  );
+  res.send(req.body.user_name);
+});
+
+app.listen(PORT, () => {
+  console.log('Example app listening on port 3000!');
+});
