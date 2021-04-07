9192173ae893eafea3ea18401c826ebf060db8aa
sqz777 <sqz777isme@gmail.com>
2021-04-07 22:04:38 +0800

    let app.js use handleMessageReactionAdd

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const igotalldayService = require('./lib/igotallday/igotalldayYoutubeService');
 const streamerLiveTimeService = require('./lib/streamer/streamerLiveTimeService');
 const { requestToMyself } = require('./lib/requestMyself');
 const { handleMessage } = require('./lib/discord/onMessage');
+const { handleReactionAdd } = require('./lib/discord/messageReactionAdd');
 
 setInterval(() => {
   requestToMyself();
@@ -62,29 +63,7 @@ Client.on('message', async (msg) => {
 });
 
 Client.on('messageReactionAdd', async (reaction, user) => {
-  // When we receive a reaction we check if the reaction is partial or not
-  if (reaction.partial) {
-    // If the message this reaction belongs to was removed the fetching
-    // might result in an API error, which we need to handle
-    try {
-      await reaction.fetch();
-    } catch (error) {
-      console.error('Something went wrong when fetching the message: ', error);
-      // Return as `reaction.message.author` may be undefined/null
-      return;
-    }
-  }
-  if (reaction.message.content.includes(user.id) && reaction.emoji.name === '💩') {
-    reaction.message.delete();
-  }
-  // Now the message has been cached and is fully available
-  console.log(
-    `${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`,
-  );
-  // The reaction is now also fully available and the properties will be reflected accurately:
-  console.log(
-    `${reaction.count} user(s) have given the same reaction to this message!`,
-  );
+  await handleReactionAdd(reaction, user);
 });
 
 Client.login(process.env.discordToken);
